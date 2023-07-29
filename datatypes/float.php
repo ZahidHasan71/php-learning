a9855758b3d04b7d7b8f57d395182b6b45d6043b
ZahidHasan71 <zahidhasan0000001@gmail.com>
2023-07-29 13:34:25 +0600

    Floating number

---
+++ datatypes/float.php
@@ -0,0 +1,15 @@
+<h1>Floating point number</h1>
+
+<?php  
+    $x = 10.465;
+    var_dump($x);   //output is - float(10.465)
+?> 
+
+<?php   
+    $dec = 44;  
+    $oct = 0243;  
+    $hexa = 0x45;  
+    echo "Decimal number: " .$dec. "</br>";  
+    echo "Octal number: " .$oct. "</br>";  
+    echo "HexaDecimal number: " .$hexa. "</br>";  
+?>  
