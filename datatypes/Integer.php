f6df25a7e7d3fa25c76dbc62d9f6517e7d9f9f0f
ZahidHasan71 <zahidhasan0000001@gmail.com>
2023-07-29 13:16:44 +0600

    Integer number

--- datatypes/Integer.php
+++ datatypes/Integer.php
@@ -4,3 +4,12 @@
 $x = 5985;
 var_dump($x);
 ?>
+
+
+//An integer data type is a non-decimal number between -2,147,483,648 and 2,147,483,647.
+ <?php
+    $y = -2,147,483,648;
+    var_dump($y);
+    $y = -2,147,483,649;
+    var_dump($y);    //increase one number then it goes on float same as posative number.
+ ?>
